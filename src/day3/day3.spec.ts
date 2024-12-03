5208ad5a2c24949799caa0f8dc11eae81ed1bbce
Aveldina <95387797+Aveldina@users.noreply.github.com>
2024-12-03 13:00:05 -0600

    Completed Day 3

--- src/day3/day3.spec.ts
+++ src/day3/day3.spec.ts
@@ -1,7 +1,16 @@
 import day3 from "./index";
 
+const sampleInput = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))";
+const expected = "161";
+
+const sampleInputPart2 = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))";
+const expectedPart2 = "48";
+
 describe("On Day 3", () => {
-  it(`part1 is identity function`, () => {
-    expect(day3.solveForPartOne("hello")).toBe("hello");
+  it(`part1 finds multiplication instructions and runs them`, () => {
+    expect(day3.solveForPartOne(sampleInput)).toBe(expected);
+  });
+  it(`part2 finds multiplication instructions and controls`, () => {
+    expect(day3.solveForPartTwo(sampleInputPart2)).toBe(expectedPart2);
   });
 });
--- src/day3/index.ts
+++ src/day3/index.ts
@@ -5,12 +5,59 @@ class Day3 extends Day {
     super(3);
   }
 
+  private findMultiplicationInstructions = (input: string, includeControl: boolean): string[] => {
+    let multiPattern: RegExp = /mul\(\d{1,},\d{1,}\)/gi;
+    if (includeControl) {
+      multiPattern = /mul\(\d{1,},\d{1,}\)|do\(\)|don't\(\)/gi;
+    }
+
+    const resultArray: string[]|null = input.match(multiPattern);
+    return resultArray ? resultArray : [];
+  };
+
+  private doMultiply(instruction: string): number {
+    const numberPattern: RegExp = /\d{1,}/g;
+    const numbers: string[] = instruction.match(numberPattern) || [];
+    return parseInt(numbers[0]) * parseInt(numbers[1]);
+  }
+
   solveForPartOne(input: string): string {
-    return input;
+    const inputLines: string[] = input.split("\r\n");
+
+    let runningCalc: number = 0;
+    
+    inputLines.forEach((line, index) => {
+      const multiInstructions = this.findMultiplicationInstructions(line, false);
+      multiInstructions.forEach((instruction) => {
+        runningCalc += this.doMultiply(instruction);
+      })
+    });
+
+    return runningCalc.toString();
   }
 
   solveForPartTwo(input: string): string {
-    return input;
+    const inputLines: string[] = input.split("\r\n");
+    let shouldMultiply: boolean = true;
+
+    let runningCalc: number = 0;
+    
+    inputLines.forEach((line, index) => {
+      const multiInstructions = this.findMultiplicationInstructions(line, true);
+      multiInstructions.forEach((instruction) => {
+        if (instruction.includes("do")) {
+          shouldMultiply = true;
+        }
+        if (instruction.includes("don")) {
+          shouldMultiply = false;
+        }
+        if (shouldMultiply && instruction.includes("mul")) {
+          runningCalc += this.doMultiply(instruction);
+        }
+      })
+    });
+
+    return runningCalc.toString();
   }
 }
 
